831bdddfa54454d8f4a30c6eb089b47aedc64fb6
bricebanka-sys <bricebanka@gmail.com>
2026-01-28 01:35:32 +0100

    Bereitstellung meiner App

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -7,9 +8,9 @@ import messageRoutes from "./routes/message.routes.js"
 import userRoutes from "./routes/user.routes.js";
 import { app, server } from "./socket/socket.js";
 
+const PORT = process.env.PORT || 5001;
 
-const PORT = process.env.PORT || 5000;
-
+const __dirname = path.resolve();
 
 dotenv.config(); // Charge les variables du fichier .env
 
@@ -19,6 +20,14 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+// Middleware pour servir les fichiers statiques du Frontend
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+// Route "Wildcard" : pour toute route non-API, renvoyer l'index.html
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 // app.get("/", (req, res) => {
 //     res.send("Der Server ist bereit mesmin!");
